01866001688f999fef4df8f896b614933f11693c
Rifatul599 <RifatulIslam112230@gmail.com>
2024-09-18 02:41:40 +0600

    Check for Uppercase or Lowercase

---
+++ uppercaseorlowercase.py
@@ -0,0 +1,7 @@
+ch = input("Enter a character :")
+if (ch>='A' and ch<='Z'):
+    print("Character is Uppercase")
+elif (ch>='a' and ch<='z'):
+    print("Character is Lowercase")
+else:
+    print("Not a Character")
